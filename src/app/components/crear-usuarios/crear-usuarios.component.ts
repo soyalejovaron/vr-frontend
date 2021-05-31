5f9b60b4bb99ae38e4b5623176df74e7d5de6437
YeisonSotoG <sotogyeison@gmail.com>
2021-05-30 22:17:54 -0500

    se modificó el formulario de crear usuario

--- src/app/components/crear-usuarios/crear-usuarios.component.ts
+++ src/app/components/crear-usuarios/crear-usuarios.component.ts
@@ -35,28 +35,22 @@ export class CrearUsuariosComponent implements OnInit {
     this.esEditar();
   }
 
-  agregarEditarEmpleado() {
+  validarUid() {
     this.submitted = true;
 
     if (this.createUsuario.invalid) {
       return;
     }
 
-    if (this.uid === null) {
-      this.agregarEmpleado();
-    } else {
-      this.editarEmpleado(this.uid);
+    if (this.uid !== null) {
+      this.editarUsuario(this.uid);
     }
 
   }
 
-  agregarEmpleado() {
-    
-  }
+  editarUsuario(uid: string) {
 
-  editarEmpleado(uid: string) {
-
-    const empleado: any = {
+    const usuario: any = {
       documento: this.createUsuario.value.documento,
       displayName: this.createUsuario.value.displayName,
       role: this.createUsuario.value.role,
@@ -66,18 +60,18 @@ export class CrearUsuariosComponent implements OnInit {
 
     this.loading = true;
 
-    this._usuarioService.actualizarUsuario(uid, empleado).then(() => {
+    this._usuarioService.actualizarUsuario(uid, usuario).then(() => {
       this.loading = false;
-      this.toastr.info('El empleado fue modificado con exito', 'Empleado modificado', {
+      this.toastr.info('El usuario fue modificado con exito', 'Usuario modificado', {
         positionClass: 'toast-bottom-right'
       })
-      this.router.navigate(['/crud']);
+      this.router.navigate(['/crud']); 
     })
   }
 
 
   esEditar() {
-    this.titulo = 'Editar Empleado'
+    this.titulo = 'Editar Usuario'
     if (this.uid !== null) {
       this.loading = true;
       this._usuarioService.getUsuario(this.uid).subscribe(data => {
